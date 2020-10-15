3cf7435cd301f76cd4f834279462be1048a6b9c4
kene <kene.prz.btz@gmail.com>
2020-10-14 20:01:30 -0500

    Primer commit se crea la biblioteca generica para los services y los controllers

---
+++ commons-microservicios/src/test/java/mx/com/kene/microservicios/commons/CommonsMicroservicios1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.kene.microservicios.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsMicroservicios1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
